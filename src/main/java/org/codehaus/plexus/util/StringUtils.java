af80b5e006bd393b1f89a9aa038aece13c3cd405
Markus KARG <markus@headcrashing.eu>
2017-06-29 23:26:22 +0200

    Replaced custom code by JRE 6 method

--- src/main/java/org/codehaus/plexus/util/StringUtils.java
+++ src/main/java/org/codehaus/plexus/util/StringUtils.java
@@ -158,7 +158,7 @@ public class StringUtils
      */
     public static boolean isNotEmpty( String str )
     {
-        return ( ( str != null ) && ( str.length() > 0 ) );
+        return ( ( str != null ) && ( !str.isEmpty() ) );
     }
 
     /**
@@ -174,7 +174,7 @@ public class StringUtils
      */
     public static boolean isEmpty( String str )
     {
-        return ( ( str == null ) || ( str.trim().length() == 0 ) );
+        return ( ( str == null ) || ( str.trim().isEmpty() ) );
     }
 
     /**
